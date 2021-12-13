a15229fa12d44606c65d5cdd9c11103e20fa5aa7
Simon Dohmen <noerk.sdo@gmail.com>
2021-12-13 14:45:09 +0100

    .

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,24 @@
 /// <reference path="../node_modules/@workadventure/iframe-api-typings/iframe_api.d.ts" />
 
-import {bootstrapExtra} from "@workadventure/scripting-api-extra";
+import { bootstrapExtra } from "@workadventure/scripting-api-extra";
 
 // The line below bootstraps the Scripting API Extra library that adds a number of advanced properties/features to WorkAdventure.
 bootstrapExtra().catch(e => console.error(e));
+
+WA.chat.sendChatMessage('Hello world', 'Mr Robot');
+
 
 let currentPopup: any = undefined;
 const today = new Date();
 const time = today.getHours() + ":" + today.getMinutes();
 
-WA.room.onEnterZone('clock', () => {
+
+
+WA.room.onEnterZone('nyan', () => {
     currentPopup =  WA.ui.openPopup("clockPopup","It's " + time,[]);
 })
 
-WA.room.onLeaveZone('clock', closePopUp)
+WA.room.onLeaveZone('nyan', closePopUp)
 
 function closePopUp(){
     if (currentPopup !== undefined) {
